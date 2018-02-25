9274288d8e01fa589a344e153b8b58c56d3dd20b
kojaniydracon <kojaniydracon@gmail.com>
2018-02-25 20:41:28 +0300

    exchanged

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    let exchange = {};
+    if (!currency) return exchange;
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    let nominals = {H: 50, Q: 25, D: 10, N: 5, P: 1};
+
+    for (let nom in nominals){
+        while(currency >= nominals[nom]){
+            exchange[nom] = exchange[nom] || 0;
+            exchange[nom] += 1;
+            currency -= nominals[nom];
+            // console.log(nom + ": " + nominals[nom]);
+        }
+    }
+
+    return exchange;
+};
